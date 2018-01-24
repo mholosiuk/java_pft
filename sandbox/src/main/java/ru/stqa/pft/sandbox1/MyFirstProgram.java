140a5f63d814faa4b260669b912eab39695acdba
mholosiuk <35525898+mholosiuk@users.noreply.github.com>
2018-01-24 18:27:16 +0200

    Added few functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstProgram.java
@@ -1,7 +1,31 @@
 package ru.stqa.pft.sandbox1;
 
 public class MyFirstProgram {
+
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        hello("world");
+        hello("user");
+        hello("Alex");
+
+        double l = 5;
+        System.out.println("Площадь квадрата " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь квадрата " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello " + somebody + "!");
+
+    }
+    public static double area (double len) {
+        return len * len;
+
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
+
     }
 }
