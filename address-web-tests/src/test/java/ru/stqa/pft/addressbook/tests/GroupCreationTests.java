bf3ad55908b1a981a762573144f2c788de5494c8
mholosiuk <35525898+mholosiuk@users.noreply.github.com>
2018-02-14 18:40:18 +0200

    Used classes and compare

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.awt.*;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
+        List <GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        List <GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
