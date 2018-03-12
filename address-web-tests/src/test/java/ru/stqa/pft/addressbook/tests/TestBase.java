eabf631946bc1cfa6e4a320a5d9bf6b22c65809f
mholosiuk <35525898+mholosiuk@users.noreply.github.com>
2018-03-12 16:06:29 +0200

    Used the protocolling with the Library logback

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -11,8 +13,12 @@ import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import javax.sql.rowset.BaseRowSet;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase{
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -23,13 +29,23 @@ public class TestBase{
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
   public ApplicationManager getApp() {
     return app;
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test" + m.getName());
   }
 
 }
